45823188f16d57f2ada4b9746423255ac658bb5d
Ritwik <ritwikchowdhury1010@gmail.com>
2022-06-22 00:46:58 +0530

    component updated

--- FrontEnd/result_management/src/app/app.module.ts
+++ FrontEnd/result_management/src/app/app.module.ts
@@ -4,15 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HomeComponent } from './home/home.component';
+import { TeacherLoginComponent } from './teacher-login/teacher-login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { TeacherComponent } from './teacher/teacher.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    HomeComponent
+    HomeComponent,
+    TeacherLoginComponent,
+    SignUpComponent,
+    TeacherComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
