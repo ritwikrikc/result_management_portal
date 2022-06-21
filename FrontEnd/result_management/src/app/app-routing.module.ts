ed287fa3fcb6079ae386824b51042a65dabe4e20
Ritwik <ritwikchowdhury1010@gmail.com>
2022-06-22 00:52:02 +0530

    teacher route added

--- FrontEnd/result_management/src/app/app-routing.module.ts
+++ FrontEnd/result_management/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TeacherLoginComponent } from './teacher-login/teacher-login.component';
 import {  SignUpComponent } from './sign-up/sign-up.component';
+import { TeacherComponent } from './teacher/teacher.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'teacher/login', component: TeacherLoginComponent },
   { path: 'signup', component: SignUpComponent},
+  { path: 'teacher', component: TeacherComponent },
   
 ]
 @NgModule({
