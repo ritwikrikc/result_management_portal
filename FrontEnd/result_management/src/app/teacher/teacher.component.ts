da376e90c3e8fae049e79a5b9db04f651407d632
Ritwik <ritwikchowdhury1010@gmail.com>
2022-06-23 01:30:40 +0530

    teacher dashboard updated

--- FrontEnd/result_management/src/app/teacher/teacher.component.ts
+++ FrontEnd/result_management/src/app/teacher/teacher.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { STUDENTS } from '../mock-student';
+import { Student } from '../student';
+import { StudentService } from '../services/student.service';
 
 @Component({
   selector: 'app-teacher',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeacherComponent implements OnInit {
 
-  constructor() { }
+  students: Student[] = [];
+
+  constructor(private studentService: StudentService) { }
 
   ngOnInit(): void {
+    this.getStudents();
+  }
+
+  getStudents(): void{
+    this.studentService.getStudents().subscribe(students => this.students = students);
   }
 
 }
