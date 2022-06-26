83e42db1c7782261de9554b97498139713d15ce4
Ritwik <ritwikchowdhury1010@gmail.com>
2022-06-26 10:25:42 +0530

    Base route added

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -8,14 +8,17 @@ const { mongoose } = require("./db-connection");
 
 
 // body parser
+const bodyParser = require("body-parser");
 
 // express app
 const app = express();
 
+// import routes
+const studentRoutes = require("./routes/students-routes");
 
 // middleware
-
-
+app.use(bodyParser.json());
+app.use("/api/student", studentRoutes);
 
 
 // routes
