00fb78a069d74cd6c775408f7b8e52b7987db71b
Ritwik <ritwikchowdhury1010@gmail.com>
2023-06-24 21:44:37 +0530

    demo sign up added

--- FrontEnd/result_management/src/app/sign-up/sign-up.component.ts
+++ FrontEnd/result_management/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators  } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
+  /* signup formcontrol fields */
+  
+    firstName= new FormControl('');
+    lastName= new FormControl('');
+    emailFormControl = new FormControl('', [Validators.email, Validators.required]);
+    passwordFormControl = new FormControl('', Validators.required);
+  
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+   /*
+   * signup teacher with name, email and password 
+   */
+   teacherSignup(){
+    console.log("firstName update", this.firstName?.value);
+    console.log("LastName update", this.lastName?.value);
+    console.log("emailFormControl update", this.emailFormControl?.value);
+    console.log("passwordFormControl update", this.passwordFormControl?.value);
+    
   }
 
 }
